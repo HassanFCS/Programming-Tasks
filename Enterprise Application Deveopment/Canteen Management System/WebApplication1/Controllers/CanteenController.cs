ade1c87ad366fa0156858e269d54abfdd27b69ae
HassanFCS <44069225+HassanFCS@users.noreply.github.com>
2020-07-16 17:56:56 +0500

    Add files via upload

---
+++ Enterprise Application Deveopment/Canteen Management System/WebApplication1/Controllers/CanteenController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class CanteenController : Controller
+    {
+        // GET: Canteen
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Index1()
+        {
+            return View();
+        }
+
+        public ActionResult Index3()
+        {
+            return View();
+        }
+
+        public ActionResult Index4()
+        {
+            return View();
+        }
+
+        public ActionResult Index5()
+        {
+            return View();
+        }
+
+        public ActionResult Index6()
+        {
+            return View();
+        }
+    }
+}
